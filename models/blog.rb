89af33809f854537d4a20a7d761b38a46486ff47
Daniel Watson <dan@dotnetguy.co.uk>
2011-02-14 21:18:58 +0000

    add theme switch for blog based upon theme key in config.yaml - have taken the view to always include jquery and pretty forms and just allow the author to overide the styles for the rest of the site

--- models/blog.rb
+++ models/blog.rb
@@ -9,6 +9,10 @@ class Blog
   def self.akismet_key
     configuration['akismet_key']
   end
+  
+  def self.theme
+    configuration['theme']
+  end
   private 
   def self.configuration
       YAML.load_file(File.join(Dir.pwd, 'config', 'config.yaml'))["#{settings.environment.to_s}"]
