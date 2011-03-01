8ce5de76913e4ebb83f350715ce52e21ec5a4abe
Daniel Watson <dan@dotnetguy.co.uk>
2011-03-01 22:58:21 +0000

    Re-factor to the add comment method to merge the hash rather than lots of L2R code

--- app/shout_mouth.rb
+++ app/shout_mouth.rb
@@ -15,17 +15,17 @@ require Dir.pwd + '/app/api/metaweblog'
 require Dir.pwd + '/app/lib/fixnum'
 
 class ShoutMouth < Sinatra::Base
-    include Metaweblog
-    
+  include Metaweblog
+
   set :public, File.dirname(__FILE__) + '/../public'
   set :views, File.dirname(__FILE__) + '/views'
-  
+
   get '/' do
     prepend_title("Home")
     @articles = Post.all_active_posts.all(:limit => Blog.posts_on_home_page.to_i)
     haml :index
   end
-  
+
   #DRY - This route matches for both pages and posts as the program flow is the same
   ["/page/:slug", "/post/:year/:month/:day/:slug"].each do |path|
     get path do
@@ -36,145 +36,135 @@ class ShoutMouth < Sinatra::Base
         redirect "/#{params[:slug]}"
       end
 
-      prepend_title(@article.title)      
-      haml :page  
+      prepend_title(@article.title)
+      haml :page if @article.is_page?
+      haml :post
     end
   end
-  
+
   post '/post/:slug/add_comment' do
-      content_type :json
-      
-      comment = params[:comment]
-      
-      post = Post.find(:persisted_slug => params[:slug]).first
-      
-      comment = Comment.new(:comment_author => comment['comment_author'], 
-       :comment_author_email => comment['comment_author_email'],
-       :comment_content => comment['comment_content'],
-       :comment_author_url => comment['comment_author_url'],
-       :user_ip => request.ip, 
-       :user_agent => request.user_agent, 
-       :referrer => request.referer,
-       :post => post)
-       
-       if comment.save 
-         comment.to_simple_comment.to_json
-       else
-         #even if the comment is marked as spam send it back so it will show on the page to satisfy the spammer / bot has
-         #done its job correctly
-         comment.errors.map{|error| {:error => error}}.to_json
-       end
-        
+    content_type :json
+    comment = Comment.new(params[:comment].merge(
+    {
+      :user_ip => request.ip,
+      :user_agent => request.user_agent,
+      :referrer => request.referer,
+      :post => Post.find(:persisted_slug => params[:slug]).first
+    }))
+    #even if the comment is marked as spam send it back so it will show on the page to satisfy the spammer / bot has
+    #done its job correctly
+    comment.save ? comment.to_simple_comment.to_json :  comment.errors.map{|error| {:error => error}}.to_json
   end
-  
+
+
   get '/category/:category' do
     prepend_title(params[:category])
     @posts = Category.posts_for_category(params[:category])
     haml :archive
   end
-  
+
   get '/tag/:tag' do
     prepend_title(params[:tag])
     @posts = Tag.posts_for_tag(params[:tag])
-    haml :archive 
-  end 
-  
+    haml :archive
+  end
+
   get '/posts/date/:year-:month' do
     prepend_title("Posts for #{params[:month]}-#{params[:year]}")
     @posts = Post.all_active_posts.all(:year => params[:year], :month => params[:month])
     haml :archive
   end
-  
+
   get '/archive' do
     prepend_title("Archive")
     @posts = Post.all_active_posts
     haml :archive
   end
-  
+
   get '/rss' do
     content_type 'text/xml'
     @posts = Post.all_active_posts
     builder :rss, :layout => false
   end
-  
+
   get '/sitemap.xml' do
     content_type 'text/xml'
     @articles = Post.all_active
     builder :sitemap, :layout => false
   end
-  
+
   get '/rsd.xml' do
     content_type 'text/xml'
     builder :rsd, :layout => false
   end
-  
+
   #This method is to allow metaweblog clients to pick up the theme of the blog
   #Minimal amount of information supplied for the user and post just to get the template to render
   get '/webpreview.html' do
-      @article = Post.new(:title => "{post-title}", 
-                          :body => "{post-body}", 
-                          :is_page => false, 
-                          :created_at => DateTime.now,
-                          :tags => ["tag1", "tag2"],
-                          :user => User.new(:firstname => "Shout", :lastname => "Mouth"))
-      haml :preview
+    @article = Post.new(:title => "{post-title}",
+    :body => "{post-body}",
+    :is_page => false,
+    :created_at => DateTime.now,
+    :tags => ["tag1", "tag2"],
+    :user => User.new(:firstname => "Shout", :lastname => "Mouth"))
+    haml :preview
   end
-  
-  get %r{/xmlrpc([*.[a-z]/]+)} do 
-      "XML-RPC server accepts POST requests only."
+
+  get %r{/xmlrpc([*.[a-z]/]+)} do
+    "XML-RPC server accepts POST requests only."
   end
-  
-  # Catch All 
+
+  # Catch All
   get '/404' do
     haml :not_found
   end
-  
+
   get '/*' do
     legacy_route = LegacyRoute.first(:slug => params[:splat])
     redirect legacy_route.post.permalink, 301 unless legacy_route.nil?
     redirect '/404'
   end
-  
-  
+
+
   #----------------------------------------------------------------------------------#
   #-------------Metaweblog/Blogger/WordPress API-------------------------------------#
   #------send: see methods in the metaweblog api module------------------------------#
   #----------------------------------------------------------------------------------#
   # MATCHES - /xmlrpc/ - /xmlrpc.{anything}
-  
-  post %r{/xmlrpc([*.[a-z]/]+)} do 
+
+  post %r{/xmlrpc([*.[a-z]/]+)} do
     #generate the xml
-    xml =  load_xml_from_request(@request.body.read, @request.params) 
+    xml =  load_xml_from_request(@request.body.read, @request.params)
     #puts "xmlpassed: #{xml}"
     #create xmlrpc request call
     call = XMLRPC::Marshal.load_call(xml)
-    
+
     # convert *.getPost to get_post
     method = call[0].gsub(/(.*)\.(.*)/, '\2').gsub(/([A-Z])/, '_\1').downcase
-    
+
     # if the payload is empty raise an error back to the client
     halt 200, {'Content-Type' => 'text/xml'}, raise_xmlrpc_error("no information has been sent") if xml.empty?
-    
+
     # get the autentication details see - metaweblog module method
     authentication_details = authentication_details_lookup(method, call)
-    
+
     #if authentication fails inform the client
     halt 200, {'Content-Type' => 'text/xml'}, raise_xmlrpc_error("User credentials supplied are incorrect") unless authenticated?(authentication_details) || does_not_need_authentication?(method)
-    
+
     #if everything with the request is fine send the payload onto the method in the metaweblog module
     response.headers['Content-Type'] = 'text/xml;'
     send(method, call)
-  end  
-  
-  private 
-  def load_xml_from_request request_body, request_params
-      if request_body.empty?
-        hash = request_params
-        request_body = (hash.keys + hash.values).join
-      end
-      return request_body
   end
-  
+
+  private
+  def load_xml_from_request request_body, request_params
+    if request_body.empty?
+      hash = request_params
+      request_body = (hash.keys + hash.values).join
+    end
+    return request_body
+  end
+
   def authenticated?(authentication_details)
     user = User.find_user(authentication_details[:username])
     if user
@@ -183,30 +173,26 @@ class ShoutMouth < Sinatra::Base
       false
     end
   end
-  
+
   helpers do
-      def partial (template, locals = {})
-        haml(template, :layout => false, :locals => locals)
-      end
-      
-      def title
-          @title.nil? ? Blog.site_name : @title
-      end
-      
-      def prepend_title title
-        @title = "#{title} - #{Blog.site_name}"
-      end
+    def partial (template, locals = {})
+      haml(template, :layout => false, :locals => locals)
+    end
 
-      def all_tags
-        Tag.all_tags
-      end
+    def title
+      @title.nil? ? Blog.site_name : @title
+    end
 
-      def month_roll
-        Post.month_year_counter
-      end
+    def prepend_title title
+      @title = "#{title} - #{Blog.site_name}"
+    end
+
+    def all_tags
+      Tag.all_tags
+    end
+
+    def month_roll
+      Post.month_year_counter
+    end
   end
-  
-
-  
-    
 end
