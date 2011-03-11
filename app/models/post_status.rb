6b9d1e175c9b7e6c95ca932687d0f0f17604b5f1
Daniel Watson <dan@dotnetguy.co.uk>
2011-03-11 19:58:02 +0000

    Nemerous bug fixes to the wordpress api - due to the api documentation not matching the real implementation of wordpress - safley say posts and comments are now fixed

---
+++ app/models/post_status.rb
@@ -0,0 +1,22 @@
+class PostStatus
+  def self.statuses
+    {
+      :publish => "Published", 
+      :draft => "Draft"
+    }
+  end
+  
+  def self.boolean_from_status(status)
+    case
+      when status == "publish"
+        true
+      else
+        false
+    end
+  end
+  
+  def self.status_from_boolean(active)
+    active ? "publish" : "draft"
+  end
+  
+end
