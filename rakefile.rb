28a4ef89dbbbd7f95d9b32dcf58e4afaa49fec08
Daniel Watson <dan@dotnetguy.co.uk>
2011-03-17 16:28:32 +0000

    Bug fix on database creation permissions for sqlite

--- rakefile.rb
+++ rakefile.rb
@@ -4,9 +4,11 @@ namespace :db do
   require 'dm-migrations'
   require File.dirname(__FILE__) + '/app/shout_mouth.rb'
   require File.dirname(__FILE__) + '/tests/test_data/test_data_helper'
+  
   desc "Create The Database"
   task :create do |t, args|
     DataMapper.auto_migrate!
+    Rake::Task["db:sqlite_file_permissions"].invoke
   end
 
   desc "Upgrade The Database"
@@ -28,6 +30,13 @@ namespace :db do
       TestDataHelper.valid_post2
       TestDataHelper.valid_comment
       TestDataHelper.legacy_route
+  end
+ 
+ desc "Set permissions for sqlite database and folder"
+ task :sqlite_file_permissions do |t, args|
+    #Ok this is stupid
+    File.chmod(0777, File.dirname(__FILE__) + "/db/shout_mouth.db")
+    File.chmod(0777, File.dirname(__FILE__) + "/db")
   end
 end
 
