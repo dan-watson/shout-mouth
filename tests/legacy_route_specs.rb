759c4a91182783e69c4a51fb924cb7cecbced013
Daniel Watson <dan@dotnetguy.co.uk>
2011-03-18 16:00:41 +0000

    Fixed broken test when a not found status code 404 is returned

--- tests/legacy_route_specs.rb
+++ tests/legacy_route_specs.rb
@@ -10,7 +10,7 @@ describe "Catching all legacy routes" do
 
   it "should invoke the url action that will catch all when no other matching route is defined" do
         get '/some-legacy-some-random.aspx'
-        last_response.headers["Location"].should include "404"
+        last_response.status.should == 404
   end
   
   it "should redirect a legancy post to its new url" do
