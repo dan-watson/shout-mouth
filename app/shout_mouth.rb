7057968b69cd00ed802becd6d79f318711ba2f19
Dan Watson <dan@dotnetguy.co.uk>
2011-04-18 00:36:26 +0100

    Turn off caching for 404's

--- app/shout_mouth.rb
+++ app/shout_mouth.rb
@@ -157,14 +157,17 @@ class ShoutMouth < Sinatra::Base
     legacy_route = LegacyRoute.first(:slug => params[:splat])
     redirect legacy_route.post.permalink, 301 unless legacy_route.nil?
     
+    #Dont bother caching the 404's because the webserver will not render the correct status code....
     #Breaking the cache from the gem does not work - Manual deletion
     cached_file = File.join(File.dirname(__FILE__), "..", "public", "cache", params[:splat])
     cached_file += ".html" if File.extname(cached_file) == ''
     FileUtils.rm_rf(cached_file)
     
-    #Dont bother caching the 404's because the webserver will not render the correct
-    #status code....
     status 404
+  end
+  
+  #Default Handler In Sinatra for 404 errors....
+  not_found do
     haml :not_found
   end
 
